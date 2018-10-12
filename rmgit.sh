9959046b8bd09d72c2ba79cc9bd2f1e57945fc73
huangyi <huangyi@youzan.com>
2018-10-12 17:59:36 +0800

    feat: 突然发现可以直接用branch -d来删除

--- rmgit.sh
+++ rmgit.sh
@@ -5,3 +5,17 @@ eval "git pull"
 eval "git co $1"
 eval "git merge master"
 eval "git diff master"
+
+echo "看到diff之后，你想要删除这个分支么?(y/n)"
+read Arg
+if [[ $Arg == "y" ]] || [[ $Arg == "Y" ]]; then
+    eval "git co master"
+    eval "git branch -D $1"
+    echo "删除成功:$1"
+    eval "git branch"
+elif [[ $Arg == "n" ]] || [[ $Arg == "N" ]]; then 
+    echo "不删除这个分支:$1"
+    eval "git branch"
+else
+    echo "这个是啥"
+fi
